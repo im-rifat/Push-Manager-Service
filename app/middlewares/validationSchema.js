e7f179aa84650a027812c852e900bade6de5e265
unknown <rrahman991@gmail.com>
2021-07-17 11:24:27 +0600

    username validation regex added.

--- app/middlewares/validationSchema.js
+++ app/middlewares/validationSchema.js
@@ -1,8 +1,12 @@
 const Joi = require('joi');
 
+const REGX_USER_NAME = '^[a-zA-Z0-9]+([_.-])?([a-zA-Z0-9])+$'; 
+
 function validateSignUpData(data) {
     const schema = Joi.object({
-        username: Joi.string().alphanum().min(5).max(20).trim(true).required(),
+        username: Joi.string()
+        .regex(RegExp(REGX_USER_NAME))
+        .min(5).max(20).trim(true).required(),
         email: Joi.string().email().trim(true).required(),
         password: Joi.string().min(8).trim(true).required(),
         roles: Joi.array().items(Joi.string().trim(true)).optional()
