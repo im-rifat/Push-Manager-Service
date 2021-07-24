d3a5ca654319ff7a0ee0a7a6a20a3074652caf55
unknown <rrahman991@gmail.com>
2021-07-24 20:23:07 +0600

    autocannon load testing script added.

--- app/routes/notification.routes.js
+++ app/routes/notification.routes.js
@@ -2,6 +2,5 @@ const Router = new require('express').Router();
 const notificationController = require('../controllers/notification.controllers');
 
 Router.post('/', notificationController.createNotification);
-Router.post('/send', notificationController.sendNotification);
 
 module.exports = Router;
