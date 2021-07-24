b5dec5da755e69efe39cf35f3b0d518409db13d9
unknown <rrahman991@gmail.com>
2021-07-24 16:02:34 +0600

    removed sendnotification function.

--- app/controllers/notification.controllers.js
+++ app/controllers/notification.controllers.js
@@ -9,7 +9,7 @@ const Notification = db.notification;
 
 const FCM_URI = 'https://fcm.googleapis.com/fcm/send';
 
-sendNotification1 = async (notification, res, next) => {
+sendNotification = async (notification, res, next) => {
 
   let options;
 
@@ -83,46 +83,14 @@ createNotification = async (req, res, next) => {
   }
 
   if(notification) {
-    await sendNotification1(notification, res, next);
-    return 
+    await sendNotification(notification, res, next);
+    return;
   }
 
   return next(new error.ApiError(StatusCodes.StatusCodes.INTERNAL_SERVER_ERROR));
 
 };
 
-sendNotification = async (req, res, next) => {
-    const uri = 'https://fcm.googleapis.com/fcm/send';
-
-    const options = {
-        headers: {'Content-Type': 'application/json',
-    'Authorization': 'key=AAAAQJFYZKQ:APA91bGSG7PjJyQcRavnf1BXUVyo9vH5olD3T73Doxivf6sOR9semXdZw6FEX2WcfZMSKRqraMwke1msz8cNvPmfs9SN8xugVH20GYY43iDzVbS9rfbFwQPPNl8kfpKQHif9DzU1JrDm'}
-      };
-
-    try {
-        let data = await axios.post(uri, {
-            "to": "coDdcmCgTjyPTj4NM3FuF3:APA91bGmNC8k4CmxrRKuFq7b8m3Gt1T5gh5wUSu2LHdFUn2ijYdB8GiGYHISOjr4EkT7MKINJMwRQEdMwafMqVcMv_WcqIakjOfEP-r51yyxb64d5rBdbMTpQ8aQG9INwUWk5Caxb8wA",
-            "notification": {
-              "title": "Breaking News",
-              "body": "New news story available."
-            },
-            "data": {
-              "story_id": "story_12345"
-            }
-          }, options);
-          
-          res.send({
-              status: data.status,
-              statusText: data.statusText,
-              data: data.data
-          });
-    } catch(err) {
-        next(err);
-    }
-
-}
-
 module.exports = {
-    createNotification,
-    sendNotification
+    createNotification
 };
